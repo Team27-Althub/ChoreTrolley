acd876ea9f817c4b09b635e4d6da5e9e17cd06af
marvellous <comlaf2001@gmail.com>
2025-07-27 23:12:41 +0100

    cart page done cart system implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { CartProvider } from 'react-use-cart';
+import CartProviderWrapper from "./cartprovider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+// function CartProviderWrapper({ children }) {
+//   return <CartProvider>{children}</CartProvider>;
+// }
 
 export const metadata: Metadata = {
   title: "ChoreTrolley",
@@ -27,7 +33,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <CartProviderWrapper>
+          {children}
+        </CartProviderWrapper>
       </body>
     </html>
   );
