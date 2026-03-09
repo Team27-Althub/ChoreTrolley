aa9724369be766047ccd5cfebd93e2b31409a708
marvellous <comlaf2001@gmail.com>
2026-03-09 17:13:46 +0100

    done

--- src/app/components/Major/Navbar.tsx
+++ src/app/components/Major/Navbar.tsx
@@ -8,25 +8,9 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between py-5 backdrop-blur-sm  top-0 z-50 items-center px-2 md:px-8 bg-gradient-to-r from-[#013328] to-[#014937]'>
-      {/* <Image src={logo} alt="Logo" width={150} height={40} /> */}
-      <div className='bg-[#f54201] font-semibold rounded-full text-white py-3 px-6'>
-       Logo here
-      </div> 
-        <div className='bg-[#fff] font-semibold rounded-full text-black py-3 px-6'>
-               <div className='flex gap-10 justify-between'>
-                <div className='hover:border-b-[#093799] hover:border-r-[#093799] hover:border-b hover:border-r px-3 transition-all duration-200 ease-in'>
-                    <Link href={''}>Our Team</Link>
-                </div>
-                <div className='hover:border-b-[#093799] hover:border-r-[#093799] hover:border-b hover:border-r px-3 transition-all duration-200 ease-in'>
-                    <Link href={''}>Services</Link>
-                </div>
-                <div className='hover:border-b-[#093799] hover:border-r-[#093799] hover:border-b hover:border-r px-3 transition-all duration-200 ease-in'>
-                    <Link href={''}>FAQs</Link>
-                </div>
-               </div>
-              </div>
-      <div className='flex justify-between items-center gap-2 md:gap-4 text-[20px]'>
+    <div className='flex justify-between py-5 backdrop-blur-sm sticky top-0 z-50 items-center px-2 md:px-8 bg-gradient-to-r from-[#013328] to-[#014937]'>
+      <Image src={logo} alt="Logo" width={150} height={40} />
+      <div className='flex justify-between items-center gap-2 md:gap-8 text-[20px]'>
          {/* <ShoppingCartIcon size={32} color='#fff'/> */}
         <Link href='/login'>
          <MenuButton buttonType='Login' />
